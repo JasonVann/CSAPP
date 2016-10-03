41c9b64320635c30c5728117f07e3b214a4a234a
JasonVann <jasonvanet@gmail.com>
2016-10-03 00:37:47 -0700

    Added show_byte and makefile

--- CSAPP.c
+++ CSAPP.c
@@ -3,9 +3,39 @@
 // Restarted on 2016-10-1
 
 #include <stdio.h>
+#include <string.h>
+
+typedef unsigned char *byte_pointer;
+//typedef double *byte_pointer;
+
+void show_bytes(byte_pointer start, int len) {
+    int i;
+    for (i = 0; i < len; i++)
+        printf(" %.2x", start[i]);
+    printf("\n");
+}
+
+void show_int(int x){
+    show_bytes((byte_pointer) &x, sizeof(int));
+}
+
+void show_pointer(void *x){
+    show_bytes((byte_pointer) &x, sizeof(void *));
+}
 
 int main()
 {
 	printf("Hello, world\n");
-	return 0;
+    int i = 200 * 300 * 400 * 500;
+    printf("i is %i\n",i);
+    double d =  4 * 500;
+    printf("d is %f\n", d);
+    int ival = 12345;
+    show_int(ival);
+    
+    show_pointer(&ival);
+    const char *s = "abcdef";
+    show_bytes((byte_pointer) s, strlen(s));
+    
+    return 0;
 }
