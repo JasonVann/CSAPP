56cbdd6e587250235f926c870a9ebea4171fd116
JasonVann <jasonvanet@gmail.com>
2017-03-15 09:38:02 +0800

    Added implementation of ll_cycle

--- CS61C/Labs/Lab1/ll_cycle.c
+++ CS61C/Labs/Lab1/ll_cycle.c
@@ -7,6 +7,15 @@ typedef struct node {
 
 int ll_has_cycle(node *head) {
 	/* your code here */
+    node* hare = head;
+    node* tortoise = head;
+    while (hare != NULL && hare->next != NULL){
+        hare = hare->next->next;
+        tortoise = tortoise->next;
+        if (hare == tortoise)
+            return 1;
+    }
+    return 0;   
 }
 
 void test_ll_has_cycle(void) {
